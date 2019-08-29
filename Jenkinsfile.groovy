eb5a121db46ee71d97cd25ba31bdbe6afa227b91
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-28 19:27:35 -0500

    TEST

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,26 +1,26 @@
 node{
     stage("Update jenkins"){
-        properties([parameters([string(defaultValue: '54.183.196.208', description: 'this is for dev', name: 'ENVIR', trim: true)])])
+        properties([parameters([string(defaultValue: '54.183.196.208', description: 'Please provide IP', name: 'ENVIR', trim: true)])])
         sh "echo Parameter added"
     }
     stage("Install git"){
-        sh "ssh ex2-user@${ENVIR} sudo yum install git  python-pip -y"
-    }
-    stage("Pull Repo"){
-         sh "ssh ec2-user@${ENVIR} git clone https://github.com/miguelgrinberg/flask-examples.git"
+        sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
     }
     stage("Remove repo"){
-         sh "ssh ex2-user@${ENVIR} sudo rm -rf /home/ec2-user/flask-examples"   
+        sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/flask-examples"
+    }
+    stage("Pull Repo"){
+        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/Nurjan87/flask-examples.git"
     }
     stage("Install Requirements"){
-       // sh "virtualenv /tmp/venv"
-       // sh ". /tmp/venv/bin/activate"
-       sh "echo Hello"
+        //sh "virtualenv /tmp/venv"
+        //sh ". /tmp/venv/bin/activate"
+        sh "echo Hello"
     }
     stage("Pip Install"){
-        sh "ssh ec2-user@${ENVIR} sudo pip install -r  ~/flask-examples/requirements.txt"
+        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/flask-examples/requirements.txt"
     }
     stage("Run App"){
-        sh "python ~/flask-examples/01-hello-world/hello.py"
+        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/flask-examples/01-hello-world/hello.py"
     }
 }
