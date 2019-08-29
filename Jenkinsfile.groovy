909718d2ce72ef449a64d586d81e3d604ab94752
Dastan Kadyrkulov <dastankadyrkulov@Dastans-MacBook-Pro.local>
2019-08-28 19:05:56 -0500

    test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,13 +1,13 @@
 node{
     stage("Update jenkins"){
-        properties
+        properties([parameters([string(defaultValue: '54.183.196.208', description: 'this is for dev', name: 'ENVIR', trim: true)])])
         sh "echo Parameter added"
     }
     stage("Install git"){
-        sh "ssh ex2-user@${Environment} sudo yum install git  python-pip -y"
+        sh "ssh ex2-user@${ENVIR} sudo yum install git  python-pip -y"
     }
     stage("Pull Repo"){
-         sh "ssh ec2-user@${Environment} git clone https://github.com/miguelgrinberg/flask-examples.git"
+         sh "ssh ec2-user@${ENVIR} git clone https://github.com/miguelgrinberg/flask-examples.git"
     }
     stage("Install Requirements"){
        // sh "virtualenv /tmp/venv"
@@ -15,7 +15,7 @@ node{
        sh "echo Hello"
     }
     stage("Pip Install"){
-        sh "ssh ec2-user@${Environment} sudo pip install -r  ~/flask-examples/requirements.txt"
+        sh "ssh ec2-user@${ENVIR} sudo pip install -r  ~/flask-examples/requirements.txt"
     }
     stage("Run App"){
         sh "python ~/flask-examples/01-hello-world/hello.py"
